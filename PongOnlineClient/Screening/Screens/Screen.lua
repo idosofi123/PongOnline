0dea181a8ee796b070f99830b609d19691f5684b
Ido Michael Sofi <pikagms@gmail.com>
2019-08-15 18:55:13 +0300

    burgerz
    ay man

---
+++ PongOnlineClient/Screening/Screens/Screen.lua
@@ -0,0 +1,1 @@
+-- Screen.lua (ABSTRACT)
--- PongOnlineClient/UI/Button.lua
+++ PongOnlineClient/UI/Button.lua
@@ -1,15 +1,26 @@
+BUTTON_WIDTH = 45
+BUTTON_HEIGHT = 20
+
 local Button = {}
 Button.__index = Button
 
-function Button:new(label, x, y, callback)
+function Button:new(label, callback, x, y, w, h)
+  w = w or BUTTON_WIDTH
+  h = h or BUTTON_HEIGHT
   local instance = {
     _label = label,
+    _callback = callback,
     _x = x,
     _y = y,
-    _callback = callback
+    _w = w,
+    _h = h,
   }
   instance.setmetatable(self)
   return instance
+end
+
+function Button:click()
+  self._callback()
 end
 
 function Button:draw()
